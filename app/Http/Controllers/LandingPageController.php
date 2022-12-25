f999f6d4e46a8910ed700e7222580aad326ffba0
syaifuddinzuhri <mochammadsyaifuddinz@gmail.com>
2022-12-26 03:43:25 +0700

    update index

--- app/Http/Controllers/LandingPageController.php
+++ app/Http/Controllers/LandingPageController.php
@@ -3,11 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Artisan;
 
 class LandingPageController extends Controller
 {
     public function index(Request $request)
     {
+        Artisan::call("cache:clear");
+        Artisan::call("route:cache");
+        Artisan::call("route:clear");
+        Artisan::call("config:cache");
         return view('landing-page.home');
     }
 
